f6e2de7736fb98e426b8d153b6ec12aa64a41997
IngMatias <matias.hernandez@correo.ucu.edu.uy>
2021-06-07 19:57:04 -0300

    Abstraccion mediante herencia

---
+++ src/Library/Characters/AbstractClasses/AbstractCharacter.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+namespace RoleplayGame
+{
+    public class AbstractCharacter
+    {
+        private int health = 100;
+
+        protected List<IItem> items = new List<IItem>();
+
+        public AbstractCharacter(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+        
+        public virtual int AttackValue
+        {
+            get
+            {
+                int value = 0;
+                foreach (IItem item in this.items)
+                {
+                    if (item is IAttackItem)
+                    {
+                        value += (item as IAttackItem).AttackValue;
+                    }
+                }
+                return value;
+            }
+        }
+
+        public virtual int DefenseValue
+        {
+            get
+            {
+                int value = 0;
+                foreach (IItem item in this.items)
+                {
+                    if (item is IDefenseItem)
+                    {
+                        value += (item as IDefenseItem).DefenseValue;
+                    }
+                }
+                return value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+            private set
+            {
+                this.health = value < 0 ? 0 : value;
+            }
+        }
+
+        public void ReceiveAttack(int power)
+        {
+            if (this.DefenseValue < power)
+            {
+                this.Health -= power - this.DefenseValue;
+            }
+        }
+
+        public void Cure()
+        {
+            this.Health = 100;
+        }
+
+        public void AddItem(IItem item)
+        {
+            this.items.Add(item);
+        }
+
+        public void RemoveItem(IItem item)
+        {
+            this.items.Remove(item);
+        }
+    }
+}
